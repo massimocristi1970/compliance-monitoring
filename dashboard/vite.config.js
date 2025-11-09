0aae8c3265acc2cf9ebccdb32cb8fc755e45e163
massimocristi1970 <massimo@ticktockloans.com>
2025-11-09 18:16:20 +0000

    Fix update

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,27 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/compliance-monitoring/",
-
-  // This helps the dev server
-  optimizeDeps: {
-    include: ["@azure/msal-browser"],
-  },
-
   build: {
     outDir: "dist",
     assetsDir: "assets",
-
-    // --- THIS IS THE NEW FIX ---
-    // This tells the 'npm run build' command (Rollup)
-    // how to handle the MSAL package properly.
-    commonjsOptions: {
-      include: /node_modules/,
-      transformMixedEsModules: true,
-    },
-    // --- END OF SECTION ---
   },
 });
