98be51cdcb3c2180a221dc6da520a3af3914f13e
massimocristi1970 <massimo@ticktockloans.com>
2025-11-09 20:22:53 +0000

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/compliance-monitoring/",
+  base: '/compliance-monitoring/',
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-});
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
