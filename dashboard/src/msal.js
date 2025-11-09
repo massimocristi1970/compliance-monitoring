e22115dcd9ce9d46908cd180e7c300b0ad4434ac
massimocristi1970 <massimo@ticktockloans.com>
2025-11-09 20:52:09 +0000

    Update msal.js

--- dashboard/src/msal.js
+++ dashboard/src/msal.js
@@ -1,19 +1,11 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 
-/**
- * Configuration object to be passed to MSAL instance on creation.
- */
 const msalConfig = {
   auth: {
-    // Your Application (client) ID
     clientId: "de33a5a5-0b65-47b8-a09d-fbc5d152930d",
-    
-    // Your Directory (tenant) ID.
     authority: "https://login.microsoftonline.com/ae2211da-bf1e-486f-a627-2bc81566edf7",
     
-    // --- THIS IS THE FIX ---
-    // We now point to the blank auth.html page.
-    // This stops the popup from breaking.
+    // This MUST point to the new auth.html file
     redirectUri: "http://localhost:5173/compliance-monitoring/auth.html",
   },
   cache: {
@@ -22,9 +14,6 @@ const msalConfig = {
   },
 };
 
-/**
- * Scopes define the permissions your app needs.
- */
 export const loginRequest = {
   scopes: ["User.Read", "Files.ReadWrite"],
 };
